352ca1a76055d0bb6f9c01a4952809b23b984bd0
Pawel Wesolowski <pawel.wesolowski@kantega.no>
2022-03-30 21:25:15 +0200

    forgot to push fix to the agree bug

--- tests/example-kantega.spec.ts
+++ tests/example-kantega.spec.ts
@@ -2,21 +2,23 @@ import { test, expect, Page } from '@playwright/test';
 
 
 test.describe('Kantega is the best company', () => {
-  test('should be found by Google as first hit', async ({ page }) => {
+  test.only('should be found by Google as first hit', async ({ page }) => {
     await page.goto("https://www.google.com/");
 
-    // Click button:has-text("I agree")
-    await Promise.all([
-      page.waitForNavigation(/*{ url: 'https://www.google.com/' }*/),
-      //page.locator('button:text-matches("Godta","i")').click()
+    const agreeButton = await page.$('text=/(Godta)|(I agree)/');
+    if(agreeButton){
+      await Promise.all([
+        page.waitForNavigation(/*{ url: 'https://www.google.com/' }*/),
+        //page.locator('button:text-matches("Godta","i")').click()
         page.locator('text=/(Godta)|(I agree)/').click()
-    ]);
+      ]);
+    }
 
     await page.fill('input[type=text]', 'kantega');
 
     // Press Enter
     await Promise.all([
-      page.waitForNavigation(/*{ url: 'https://www.google.com/search?q=fiskeridirektoratet&source=hp&ei=6uyUYczRGMmExc8PgpKZiAE&iflsig=ALs-wAMAAAAAYZT6-v5CJFd3Mu4JJy_d2i98EZWlQRi0&oq=kantega&gs_lcp=Cgdnd3Mtd2l6EAMyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUILhCABDIFCAAQgAQyBQgAEIAEOgsILhCABBDHARDRAzoLCC4QgAQQxwEQowI6CwguEIAEEMcBEK8BOgcILhCABBAKOgcIABCABBAKUMgLWKUaYPolaAFwAHgAgAGdAYgBjgWSAQM1LjKYAQCgAQGwAQA&sclient=gws-wiz&ved=0ahUKEwjM7q7gqp_0AhVJQvEDHQJJBhEQ4dUDCAc&uact=5' }*/),
+      page.waitForNavigation(),
       page.press('input[type=text]', 'Enter')
     ]);
 
